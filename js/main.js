bb8b2b511125aaa90e1b3a77ca4ad938ce54b5e9
Ahmed Afifi <ahmed.mohsem.afifi@gmail.com>
2019-05-03 17:16:25 +0400

    Contact and Footer Completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 // GET the current Year of the copyright
 $("#year").text(new Date().getFullYear());
+
+// INIT SCROLL SPY
+$("body").scrollspy({
+    target: "#main-nav"
+});
+
+// SMOOTH SCROLLING
+$("#main-nav a").on("click", function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function() {
+                window.location.hash = hash;
+            }
+        );
+    }
+});
